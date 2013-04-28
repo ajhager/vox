2ccc308efac8988744e595f759464c2bbcb92904
Joseph Hager <ajhager@gmail.com>
2013-04-28 17:30:58 -0400

    Update demo to use new path

--- demos/main.go
+++ demos/main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-    "hagerbot.com/vox"
-    "time"
+	"github.com/ajhager/vox"
+	"time"
 )
 
 func main() {
-    err := vox.Init("", 44100, 2, 0)
-    if err != nil {
-        panic(err)
-    }
-    defer vox.Quit()
+	err := vox.Init("", 44100, 2, 0)
+	if err != nil {
+		panic(err)
+	}
+	defer vox.Quit()
 
-    println(vox.Version)
+	println(vox.Version)
 
-    song, err := vox.Open("../data/songs/test.sunvox")
-    if err != nil {
-        panic(err)
-    }
-    defer song.Close()
+	song, err := vox.Open("../data/songs/test.sunvox")
+	if err != nil {
+		panic(err)
+	}
+	defer song.Close()
 
-    println(song.Name())
+	println(song.Name())
 
-    song.SetVolume(256)
+	song.SetVolume(256)
 
-    song.Mod[7].Trigger(0, 64, 128, 0, 0)
-    time.Sleep(1 * time.Second)
-    song.Mod[7].Trigger(0, 64, 128, 0, 0)
-    time.Sleep(1 * time.Second)
+	song.Mod[7].Trigger(0, 64, 128, 0, 0)
+	time.Sleep(1 * time.Second)
+	song.Mod[7].Trigger(0, 64, 128, 0, 0)
+	time.Sleep(1 * time.Second)
 
-    song.Play()
+	song.Play()
 
-    for !song.Finished() {
-    }
+	for !song.Finished() {
+	}
 }
